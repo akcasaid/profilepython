e727a6d3c920b8aaa307b1e9e08a9218ebb6f9fe
Said Akça <56161195+akcasaid@users.noreply.github.com>
2024-08-13 13:52:10 +0300

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,15 @@
+import time
+
+def slow_function():
+    time.sleep(2)
+
+def fast_function():
+    time.sleep(0.5)
+
+def main():
+    for _ in range(3):
+        slow_function()
+        fast_function()
+
+if __name__ == "__main__":
+    main()
